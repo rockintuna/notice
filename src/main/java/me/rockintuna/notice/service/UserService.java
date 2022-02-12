ebad059b3d4b4ee9de7cce3d9bb952f1887494e4
rockintuna <rockintuna@naver.com>
2022-02-13 00:12:29 +0900

    UserDetails 구현

---
+++ src/main/java/me/rockintuna/notice/service/UserService.java
@@ -0,0 +1,23 @@
+package me.rockintuna.notice.service;
+
+import me.rockintuna.notice.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import me.rockintuna.notice.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return UserDetailsImpl.from(user);
+    }
+}
