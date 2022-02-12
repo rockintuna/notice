cd0db6b0863f018aca50c3ca19c0a39896a036db
rockintuna <rockintuna@naver.com>
2022-02-12 17:10:53 +0900

    initialize repository

---
+++ src/test/java/me/rockintuna/notice/NoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rockintuna.notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
