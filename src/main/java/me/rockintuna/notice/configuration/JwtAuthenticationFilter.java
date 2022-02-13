44b0e30243bfb07407c1713cf6cd725898cf255a
rockintuna <rockintuna@naver.com>
2022-02-13 16:30:21 +0900

    SecurityConfig, JwtTokenProvider, JwtAuthenticationFilter

---
+++ src/main/java/me/rockintuna/notice/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package me.rockintuna.notice.configuration;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(
+            AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+        super(authenticationManager);
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain
+    ) throws IOException, ServletException {
+        Authentication authentication = getAuthentication(request);
+
+        if (authentication != null) {
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
+
+        chain.doFilter(request,response);
+    }
+
+    private Authentication getAuthentication(HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization");
+        if ( accessToken == null ) {
+            return null;
+        } else {
+            Claims claims = jwtTokenProvider.getClaims(accessToken.substring("Bearer ".length()));
+            return new UsernamePasswordAuthenticationToken(claims, null);
+        }
+    }
+}
