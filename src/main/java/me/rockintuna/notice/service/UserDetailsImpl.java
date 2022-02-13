2054a6bfa94715274635a4bfeff221b9660ebd9a
rockintuna <rockintuna@naver.com>
2022-02-13 18:48:37 +0900

    UserDetails 분리

--- src/main/java/me/rockintuna/notice/service/UserDetailsImpl.java
+++ src/main/java/me/rockintuna/notice/service/UserDetailsImpl.java
@@ -2,9 +2,12 @@ package me.rockintuna.notice.service;
 
 import me.rockintuna.notice.domain.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class UserDetailsImpl implements UserDetails{
 
@@ -16,7 +19,12 @@ public class UserDetailsImpl implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        SimpleGrantedAuthority simpleAuthority = new SimpleGrantedAuthority("USER");
+        return List.of(simpleAuthority);
+    }
+
+    public User getUser() {
+        return user;
     }
 
     @Override
--- src/main/java/me/rockintuna/notice/service/UserService.java
+++ src/main/java/me/rockintuna/notice/service/UserService.java
@@ -1,34 +1,25 @@
 package me.rockintuna.notice.service;
 
-import me.rockintuna.notice.dto.RegisterUserRequestDto;
-import me.rockintuna.notice.dto.UserLoginRequestDto;
-import me.rockintuna.notice.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
 import me.rockintuna.notice.configuration.JwtTokenProvider;
 import me.rockintuna.notice.domain.User;
+import me.rockintuna.notice.dto.RegisterUserRequestDto;
+import me.rockintuna.notice.dto.UserLoginRequestDto;
 import me.rockintuna.notice.dto.UserLoginResponseDto;
+import me.rockintuna.notice.dto.UserResponseDto;
 import me.rockintuna.notice.exception.LoginFailureException;
 import me.rockintuna.notice.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-        return UserDetailsImpl.from(user);
-    }
 
     public UserLoginResponseDto login(UserLoginRequestDto requestDto) {
         User user = userRepository.findByEmail(requestDto.getEmail())
