bc55518a41108f6990911e558aef289ad971b9c8
rockintuna <rockintuna@naver.com>
2022-02-13 16:30:51 +0900

    User 컬럼 설정 및 생성자

--- src/main/java/me/rockintuna/notice/domain/User.java
+++ src/main/java/me/rockintuna/notice/domain/User.java
@@ -1,11 +1,9 @@
 package me.rockintuna.notice.domain;
 
+import me.rockintuna.notice.dto.RegisterUserRequestDto;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -14,9 +12,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private String username;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
+
+    private User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public static User create(RegisterUserRequestDto requestDto, String encodedPassword) {
+        return new User(
+                requestDto.getUsername(),
+                requestDto.getEmail(),
+                encodedPassword);
+    }
 }
