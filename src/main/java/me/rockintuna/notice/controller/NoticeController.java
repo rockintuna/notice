89666376e8ebe1b3fea9bfbabc0ac9ec033901b1
rockintuna <rockintuna@naver.com>
2022-02-13 18:47:09 +0900

    NoticeController 반환 타입 변경

--- src/main/java/me/rockintuna/notice/controller/NoticeController.java
+++ src/main/java/me/rockintuna/notice/controller/NoticeController.java
@@ -1,10 +1,19 @@
 package me.rockintuna.notice.controller;
 
+import lombok.RequiredArgsConstructor;
+import me.rockintuna.notice.domain.File;
+import me.rockintuna.notice.domain.User;
 import me.rockintuna.notice.dto.NoticeRequestDto;
 import me.rockintuna.notice.dto.NoticeResponseDto;
+import me.rockintuna.notice.service.UserDetailsImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import service.NoticeService;
+import me.rockintuna.notice.service.NoticeService;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +24,16 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @PostMapping("/api/notice")
-    public NoticeResponseDto createNotice(@RequestBody NoticeRequestDto requestDto) {
-        User user = new User();
+    public ResponseEntity<NoticeResponseDto> createNotice(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody NoticeRequestDto requestDto)
+            throws URISyntaxException {
+        User user = userDetails.getUser();
         List<File> fileList = new ArrayList<>();
-        return noticeService.createNotice(requestDto, user, fileList);
+        NoticeResponseDto responseDto = noticeService.createNotice(requestDto, user, fileList);
+        String url = "/api/notice/"+responseDto.getId();
+
+        return ResponseEntity.created(new URI(url)).body(responseDto);
     }
 
     @GetMapping("/api/notice/{id}")
