f478b012247e50254de7004987b6e2a03e97bab3
rockintuna <rockintuna@naver.com>
2022-02-14 16:42:57 +0900

    사용자 없음 예외 추가

--- src/main/java/me/rockintuna/notice/exception/GlobalController.java
+++ src/main/java/me/rockintuna/notice/exception/GlobalController.java
@@ -2,6 +2,7 @@ package me.rockintuna.notice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +11,11 @@ public class GlobalController {
 
     @ExceptionHandler
     public ResponseEntity<?> loginFailureExceptionHandler(LoginFailureException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> usernameNotFoundExceptionHandler(UsernameNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 
